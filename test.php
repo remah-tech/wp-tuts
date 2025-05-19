5be404c37ead7fb73bb10b06758f9d48566c746f
Mohamed Gomaa <senior.mgomaa@gmail.com>
2025-05-19 21:20:15 +0300

    first session

---
+++ test.php
@@ -0,0 +1,57 @@
+<?php
+
+// $path = getcwd(); //get real path
+$path = '/var/www/html/php-tutss';
+$dir = scandir($path); //list of files and directories, return array|bool(false)
+
+/**
+ * foreach loop
+ */
+// foreach ($dir as $key => $value) 
+// {
+//     /**
+//      * if statement
+//      */
+//     if( $value!='.' && $value!='..' )
+//     {
+//         echo "Key: " . $key." => Value: ". $value. "<br />";
+//     }
+// }
+
+// if($dir)
+// {
+//     $count_of_files = count($dir);
+
+//     for($i=0; $i<$count_of_files; $i++)
+//     {
+//         echo $dir[$i] ."<br />";
+//     }
+// } else{
+//     echo "Dir: ".$path." non exists";
+// }
+
+if(!$dir)
+{
+    echo "Dir: ".$path." non exists";
+    exit;
+}
+
+$count_of_files = count($dir);
+
+for($i=0; $i<$count_of_files; $i++)
+{
+    echo $dir[$i] ."<br />";
+}
+
+// $array = [
+//     0 => 'A',
+//     1 => 'B',
+//     2 => 'C',
+// ];
+
+// echo $array[0] . $array[1] . $array[2];
+
+// for($i=0; $i<count($array); $i++)
+// {
+//     echo $array[$i];
+// }
